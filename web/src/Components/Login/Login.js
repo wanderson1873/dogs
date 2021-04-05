c793e56918b4ac025946d60dc8e662700f709104
Wanderson Oliveira <wanderson1873@live.com>
2021-04-05 18:05:30 +0100

    Create Login

---
+++ web/src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Login.module.css'
+
+const Login = () => {
+   return (
+      <div className={style.login}>
+         Login
+      </div>
+   )
+}
+
+export default Login

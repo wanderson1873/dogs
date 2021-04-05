42543a571427ea697076415afde23915cb595754
Wanderson Oliveira <wanderson1873@live.com>
2021-04-05 18:05:01 +0100

    Create Footer.js

---
+++ web/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = () => {
+   return (
+      <footer className={style.footer}>
+         Footer
+      </footer>
+   )
+}
+
+export default Footer

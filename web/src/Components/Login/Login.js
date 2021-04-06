2e7586d13219e87f4d69761401ad83e2d4ef7400
Wanderson Oliveira <wanderson1873@live.com>
2021-04-06 10:01:03 +0100

    Create Login

--- web/src/Components/Login/Login.js
+++ web/src/Components/Login/Login.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import style from './Login.module.css'
+import { Routes, Route } from 'react-router'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
+
 
 const Login = () => {
    return (
       <div className={style.login}>
-         Login
+         <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="criar" element={<LoginCreate />} />
+            <Route path="perdeu" element={<LoginPasswordLost />} />
+            <Route path="resetar" element={<LoginPasswordReset />} />
+         </Routes>
       </div>
    )
 }

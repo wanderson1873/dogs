b744d27da760c4d7865b69e6987b63246773713b
Wanderson Oliveira <wanderson1873@live.com>
2021-04-06 09:02:03 +0100

    Update Header

--- web/src/Components/Header.js
+++ web/src/Components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import style from './Header.module.css'
+import {ReactComponent as Dogs} from '../Assets/dogs.svg'
 
 const Header = () => {
    return (
       <header className={style.header}>
-         <nav>
-            <Link to="/" end>Home</Link>
-            <Link to="/login">Login / Criar</Link>
+         <nav className="container">
+            <Link to="/" className={style.logo} aria-label="Dogs - Home">
+               <Dogs />
+            </Link>
+            <Link to="/login" className={style.login}>
+               Login / Criar
+            </Link>
          </nav>
       </header>
    )

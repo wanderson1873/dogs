2c6d551b812331709c0fc605817490850d5bd3c1
Wanderson Oliveira <wanderson1873@live.com>
2021-04-10 16:55:54 +0100

    update LoginForm

--- web/src/Components/Login/LoginForm.js
+++ web/src/Components/Login/LoginForm.js
@@ -4,6 +4,9 @@ import Button from '../Form/Button'
 import Input from '../Form/Input'
 import useForm from '../../Hooks/useForm'
 import { UserContext } from '../../UserContext'
+import Error from '../Helper/Error'
+import style from './LoginForm.module.css'
+import styleBtn from '../Form/Button.module.css'
 
 const LoginForm = () => {
    const username = useForm('')
@@ -22,14 +25,20 @@ const LoginForm = () => {
    return (
       <section className="animeLeft">
          <h1 className="title">Login</h1>
-         <form action="" onSubmit={handleSubmit}>
+         <form className={style.form} onSubmit={handleSubmit}>
             <Input label="Usuáio" name="username" type="text" {...username}/>
             <Input label="Senha" name="password" type="password" {...password}/>
             
             {loading ? <Button disabled>Entrando...</Button> : <Button>Entrar</Button>}
-            {error && <p>{error}</p>}
+            <Error error={error} />
          </form>
-         <Link to="/login/criar">Cadastro</Link>
+         <Link className={style.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+         <div className={style.cadastro}>
+            <h2 className={style.subtitle}>Cadastre-se</h2>
+            <p>Ainda não possui conta? cadastre-se no site.</p>
+            <Link className={styleBtn.button} to="/login/criar">Cadastro</Link>
+         </div>
+         
       </section>
    )
 }

9defa0ec4aeb21e2c64c22c14d017e307dc233a2
Wanderson Oliveira <wanderson1873@live.com>
2021-04-10 17:26:16 +0100

    style page login

--- web/src/Components/Login/Login.js
+++ web/src/Components/Login/Login.js
@@ -13,14 +13,16 @@ const Login = () => {
 
    if(login === true) return <Navigate to="/conta"/>
    return (
-      <div className={style.login}>
-         <Routes>
-            <Route path="/" element={<LoginForm />} />
-            <Route path="criar" element={<LoginCreate />} />
-            <Route path="perdeu" element={<LoginPasswordLost />} />
-            <Route path="resetar" element={<LoginPasswordReset />} />
-         </Routes>
-      </div>
+      <section className={style.login}>
+         <div className={style.forms}>
+            <Routes>
+               <Route path="/" element={<LoginForm />} />
+               <Route path="criar" element={<LoginCreate />} />
+               <Route path="perdeu" element={<LoginPasswordLost />} />
+               <Route path="resetar" element={<LoginPasswordReset />} />
+            </Routes>
+         </div>
+      </section>
    )
 }
 

fe60b3c436a59d4f097ab0fc643117fe09ddaa42
Wanderson Oliveira <wanderson1873@live.com>
2021-04-12 18:22:33 +0100

    update title dinamico

--- web/src/Components/User/UserHeader.js
+++ web/src/Components/User/UserHeader.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import UserHeaderNav from './UserHeaderNav'
 import style from './UserHeader.module.css'
+import { useLocation } from 'react-router'
 
 const UserHeader = () => {
+   const [title, setTitle] = React.useState('');
+   const location = useLocation();
+
+   React.useEffect(() => {
+      const {pathname} = location;
+      switch(pathname){
+         case '/conta/postar':
+            setTitle('Poste sua Foto');
+            break;
+         case '/conta/estatistica':
+            setTitle('Estatísticas');
+            break;
+         default:
+            setTitle('Minha Conta');
+      }
+   }, [location])
    return (
       <header className={style.header}>
-         <h1 className="title">Titulo</h1>
+         <h1 className="title">{title}</h1>
          <UserHeaderNav />
       </header>
    )

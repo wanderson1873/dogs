608920a6b313874534a4ba88bd1f43ea17d5c89d
Wanderson Oliveira <wanderson1873@live.com>
2021-04-12 18:12:45 +0100

    Corrigir bug de error

--- web/src/Hooks/useFetch.js
+++ web/src/Hooks/useFetch.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const useFetch = () => {
    const [data, setData] = React.useState(null);
-   const [error, setError] = React.useState(false);
+   const [error, setError] = React.useState(null);
    const [loading, setLoading] = React.useState(false);
 
    const request = React.useCallback(async (url, options) => {
@@ -12,12 +12,11 @@ const useFetch = () => {
          setError(null);
          setLoading(true);
          response = await fetch(url, options);
-         console.log(response);
          json = await response.json();
          if(response.ok === false) throw new Error(json.message);
       } catch (err) {
-         json(null);
-         setError("mensagem");
+         json = null;
+         setError(err.message);
       } finally {
          setData(json);
          setLoading(false);

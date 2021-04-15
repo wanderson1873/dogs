078c50071897830fcead48a1b087b06131c25f8b
Wanderson Oliveira <wanderson1873@live.com>
2021-04-15 17:48:09 +0100

    Add feature Comments

---
+++ web/src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {ReactComponent as Send} from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+import { COMMENT_POST } from '../../api'
+import Error from '../Helper/Error'
+import style from './PhotoCommentsForm.module.css'
+
+const PhotoCommentsForm = ({id, setComments}) => {
+   const [comment, setComment] = React.useState('');
+   const {error, request} = useFetch();
+
+   async function handleSubmit(event) {
+      event.preventDefault();
+      const token = window.localStorage.getItem('token');
+      const {url, options} = COMMENT_POST(id, {comment}, token);
+      const { response, json } = await request(url, options);
+      if(response.ok) {
+         setComment('');
+         setComments((comments) => [...comments, json])
+      }
+   }
+
+   return (
+      <form className={style.form} onSubmit={handleSubmit}>
+         <textarea 
+         className={style.textarea}
+            id="comment" 
+            name="comment" 
+            placeholder='Comente...'
+            value={comment}
+            onChange={({target}) =>setComment(target.value)}
+         />
+         <button className={style.button}>
+            <Send />
+         </button>
+         <Error error={error} />
+      </form>
+   )
+}
+
+export default PhotoCommentsForm

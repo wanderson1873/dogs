a70f881611a5a2bd9db53866f5c9c822882eb4a3
Wanderson Oliveira <wanderson1873@live.com>
2021-04-18 00:34:00 +0100

    Update LoginPasswordLost.js

--- web/src/Components/Login/LoginPasswordLost.js
+++ web/src/Components/Login/LoginPasswordLost.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import Input from '../Form/Input';
+import Button from '../Form/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api'
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
+   const login = useForm('');
+   const {data, loading, error, request} = useFetch()
+
+   async function handleSubmit(event) {
+      event.preventDefault();
+      if(login.validate()) {
+         const {url, options} = PASSWORD_LOST({
+            login: login.value,
+            url: window.location.href.replace('perdeu', 'resetar')
+         })
+         request(url, options)
+      }
+   }
    return (
-      <div>
+      <section>
+         <h1 className="title">Perdeu a senha?</h1>
+         {data ? <p style={{color: '#4c1'}}>{data}</p> : (
+            <form onSubmit={handleSubmit}>
+            <Input label="Email / Usuário" type="text" name="email"  {...login}/> 
+            <Error error={error} />
+            {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar email</Button>}
+         </form>
+         )}
          
-      </div>
+            
+      </section>
    )
 }
 

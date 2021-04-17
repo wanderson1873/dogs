80340365e8cd6cd02eaa4920379a420f0957d0e4
Wanderson Oliveira <wanderson1873@live.com>
2021-04-18 00:03:11 +0100

    Update Feed.js

--- web/src/Components/Feed/Feed.js
+++ web/src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedModal from './FeedModal'
 import FeedPhotos from './FeedPhotos'
+import PropTypes from 'prop-types'
 
 const Feed = ({user}) => {
    const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -44,6 +45,17 @@ const Feed = ({user}) => {
          ))}
       </div>
    )
+}
+
+Feed.defaultProps = {
+   user: 0
+}
+
+Feed.prototype = {
+   user: PropTypes.oneOfType([
+      PropTypes.string.isRequired, 
+      PropTypes.number.isRequired
+   ])
 }
 
 export default Feed

7c431d1bdecfc2121c99df677bc6531e64958770
Wanderson Oliveira <wanderson1873@live.com>
2021-04-17 13:39:24 +0100

    Add Route /foto/:id

--- web/src/App.js
+++ web/src/App.js
@@ -7,6 +7,7 @@ import Footer from './Components/Footer'
 import { UserStorage } from './UserContext'
 import User from './Components/User/User'
 import ProtectedRoute from './Components/Helper/ProtectedRoute'
+import Photo from './Components/Photo/Photo'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="login/*" element={<Login />} />
               <ProtectedRoute path="conta/*" element={<User />} />
+              <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>

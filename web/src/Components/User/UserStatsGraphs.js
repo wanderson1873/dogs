a9796759cc37c72cac1a0ad31121479e5cc92a1f
Wanderson Oliveira <wanderson1873@live.com>
2021-04-18 11:01:04 +0100

    Add lazy

--- web/src/Components/User/UserStatsGraphs.js
+++ web/src/Components/User/UserStatsGraphs.js
@@ -1,19 +1,49 @@
 import React from 'react'
 import style from './UserStatsGraphs.module.css'
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory'
 
 const UserStatsGraphs = ({data}) => {
    const [graph, setGraph] = React.useState([]);
    const [total, setTotal] = React.useState(0);
 
    React.useEffect(() => {
-      if(data) 
+      const graphData = data.map(item => {
+         return {
+            x: item.title,
+            y: Number(item.acessos)
+         }
+      })
       setTotal(data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b))
+      setGraph(graphData)
    }, [data])
 
    return (
       <section className={`${style.graph} animeLeft`}>
-         <div className={style.total}>
+         <div className={`${style.total} ${style.graphItem}`}>
             <p>Acessos: {total} </p>
+         </div>
+         <div className={style.graphItem}>
+            <VictoryPie
+               data={graph}
+               innerRadius={50} 
+               padding={{top: 20, bottom: 20, left: 80, right: 80}}
+               style={{
+                  data: {
+                     fillOpacity: 0.9,
+                     stroke: '#fff',
+                     strokeWidth: 2
+                  },
+                  labels: {
+                     fontSize: 14,
+                     fill: '#333'
+                  }
+               }}
+            />
+         </div>
+         <div className={style.graphItem}>
+            <VictoryChart>
+               <VictoryBar alignment="start" data={graph}/>
+            </VictoryChart>
          </div>
       </section>
    )

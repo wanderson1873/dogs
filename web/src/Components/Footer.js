abf78a93f41c6ef3d4f4e3d53834e5b55ec2a823
Wanderson Oliveira <wanderson1873@live.com>
2021-04-18 10:08:53 +0100

    Add Footer

--- web/src/Components/Footer.js
+++ web/src/Components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import style from './Footer.module.css'
+import { ReactComponent as DogsFooter} from '../Assets/dogs-footer.svg'
 
 const Footer = () => {
    return (
       <footer className={style.footer}>
-         
+         <DogsFooter />
+         <p>Dogs. Alguns direitos reservados</p>
       </footer>
    )
 }
